0873837b5b5613e857da5f35a6ba40297ec63a0f
Laetitia Zammit <laetitia@navut.com>
2018-02-16 17:58:58 -0500

    light UI with sorted data presented

--- src/App.js
+++ src/App.js
@@ -1,38 +1,122 @@
 import React, { Component } from 'react';
-import Highcharts from 'highcharts';
+import { data } from './data';
+import { regionTranslator } from './utils';
+import { Menu, Icon } from 'antd';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 
 class App extends Component {
 
-  // constructor (props) {
-  //   super(props);
-  //
-  //   this.state = {
-  //   };
-  // }
+  state = {
+    current: 'us-east-1',
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  }
 
   render () {
+    const aToZ = data.record.sort((a, b) => a.displayName.localeCompare(b.displayName));
+    const arrCurrentRegion = aToZ.filter((obj) => { return obj.regionId === this.state.current; });
+
     return (
-      <div class="documentation">
-        <h1 class="logo">Elastic</h1>
-        <div class="welcome">
-          <h2>Hey, there!</h2>
-          <p>Here's a small assortment of colors you can use to complete the exercise.</p>
-          <p>Feel free to bring your own styles and get creative!</p>
-        </div>
-        <div class="palette">
-          <div class="color pink"></div>
-          <div class="color green"></div>
-          <div class="color yellow"></div>
-          <div class="color purple"></div>
-          <div class="color gray"></div>
-          <div class="color blackish"></div>
-        </div>
-        <div class="welcome">
-          <p>Remember: if you have any questions, do not hesitate to ask your interviewer!</p>
+      <div id="wrapper">
+        <div className="container">
+          <div className="row">
+            <h1>Elasticsearch clusters - Status Page</h1>
+            <div className="wrapper-regions">
+              <Menu
+                onClick={this.handleClick}
+                selectedKeys={[this.state.current]}
+                mode="horizontal"
+              >
+              {
+                this.props.regions.map((region, index) => {
+                  return (
+                    <Menu.Item key={region.id}>
+                      {region.displayName}
+                    </Menu.Item>
+                  );
+                })
+              }
+              </Menu>
+            </div>
+            <div className="page-content-wrapper">
+              <ul>
+                {
+                  arrCurrentRegion.map((cluster, index) => {
+                    return (
+                      <li key={`cluster_${index}`}>
+                        <h5>{cluster.displayName}
+                          {cluster.user.isPremium &&
+                            <Icon type="star" />
+                          }
+                        </h5>
+                        <span className={cluster.healthy ? 'health color green' : 'health color pink' }>
+                          {cluster.healthy ? 'healthy' : 'not healthy' }
+                        </span>
+                        <span className="kibana">
+                          Kibana: {cluster.kibana.enabled ? 'enabled' : 'disabled'}
+                        </span>
+                        <span className="version">
+                          {cluster.plan.version}
+                        </span>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+App.defaultProps = {
+  regions: [
+    {
+      id: "us-east-1",
+      displayName: "Virginia"
+    },
+    {
+      id: "us-west-1",
+      displayName: "N. California"
+    },
+    {
+      id: "us-west-2",
+      displayName: "Oregon"
+    },
+    {
+      id: "eu-west-1",
+      displayName: "Ireland"
+    },
+    {
+      id: "eu-central-1",
+      displayName: "Frankfurt"
+    },
+    {
+      id: "ap-southeast-1",
+      displayName: "Singapore"
+    },
+    {
+      id: "ap-southeast-2",
+      displayName: "Sydney"
+    },
+    {
+      id: "ap-northeast-1",
+      displayName: "Tokyo"
+    },
+    {
+      id: "sa-east-1",
+      displayName: "Sao Paulo"
+    }
+  ]
+};
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -17,6 +17,28 @@ export const centsToRoundedDollar = value => Math.floor((value / 100.0));
 
 export const onlyNumbers = (string = '') => string.replace(/[^0-9]/g, '');
 
-
-
+export const regionTranslator = (regionId) => {
+  switch (regionId) {
+    case 'us-east-1':
+      return 'Virginia';
+    case 'us-west-1':
+      return 'N. California';
+    case 'us-west-2':
+      return 'Oregon';
+    case 'eu-west-1':
+      return 'Ireland';
+    case 'eu-central-1':
+      return 'Frankfurt';
+    case 'ap-southeast-1':
+      return 'Singapore';
+    case 'ap-southeast-2':
+      return 'Sydney';
+    case 'ap-northeast-1':
+      return 'Tokyo';
+    case 'sa-east-1':
+      return 'Sao Paulo';
+    default:
+      return regionId;
+  }
+};
 
